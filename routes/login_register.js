9e8756d84f150329cee2b5129aff508c809f88e3
Jesse <jessedaniel@live.ca>
2021-03-06 23:27:44 +0000

    added routes

--- routes/login_register.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router  = express.Router();
-const app = express()
-
-router.get("/login", (req, res) => {
-  res.render('log_reg');
-});
-
-module.exports = router
--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,6 @@
 
 const express = require('express');
 const router  = express.Router();
-const app = express()
 
 
 
@@ -25,8 +24,16 @@ module.exports = (db) => {
       });
   });
 
-  router.get("/login", (req, res) => {
-    res.render("log_reg");
+  router.get("/user/points", (req, res) => {
+    res.render('my_points');
+  });
+
+  router.get("/user/favorites", (req, res) => {
+    res.render('my_map');
+  });
+
+  router.get("/user/profile", (req, res) => {
+    res.render('profile');
   });
 
    return router;
