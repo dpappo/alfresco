e8c03a5ff061a5075e2fae799b1fb8a69499bab9
Dan Pappo <dpappo@gmail.com>
2021-03-06 21:10:40 -0500

    named functions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -11,6 +11,25 @@ $(() => {
 
 mdc.ripple.MDCRipple.attachTo(document.querySelector('.foo-button'));
 
+
+//log out
+// const signOut = function() {
+//     var auth2 = gapi.auth2.getAuthInstance();
+//     auth2.signOut().then(function () {
+//       console.log('User signed out.');
+//     });
+
+//get user info
+const onSignIn = function(googleUser) {
+  alert("ran")
+  var profile = googleUser.getBasicProfile();
+  console.log(profile);
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+
 //maps!
 let map;
 function initMap() {
@@ -19,12 +38,3 @@ function initMap() {
     zoom: 8
   });
 };
-
-//get user info
-function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-}
