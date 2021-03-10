75e92533fb370af7a009462b206d747a8921c234
Dan Pappo <dpappo@gmail.com>
2021-03-10 14:47:05 -0500

    fix zoom, forms, mobile views

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -14,7 +14,7 @@
 
 
 //maps!
-var mymap = L.map('mapid').setView([43.6532, -79.3832], 13);
+var mymap = L.map('mapid').setView([43.6532, -79.3832], 10);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
