dd9add523467fd35c75725bf89fdf45dd68f22d8
Adam Mohammed <13.adamm@gmail.com>
2021-03-10 13:28:01 -0500

    added SEED data/ error shell page

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,6 @@
--- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+
+INSERT INTO users (name, email, password) VALUES ('admin', 'admin@admin.com', 'master');
+INSERT INTO users (name, email, password) VALUES ('admin2', 'admin2@admin.com', 'admin');
+INSERT INTO users (name, email, password) VALUES ('adam', 'adam@admin.com', 'adam');
+INSERT INTO users (name, email, password) VALUES ('dan', 'dan@admin.com', 'dan');
+INSERT INTO users (name, email, password) VALUES ('jesse', 'jesse@admin.com', 'jesse');
---
+++ db/seeds/02_locations.sql
@@ -0,0 +1,24 @@
+
+INSERT INTO locations (user_id, Description, title, address, phone, image, long, lat)
+VALUES (1, 'The best Jerk in Town', 'Chubbys Jamaican Kitchen', '104 Portland St', '416-235-1345', 'https://media.blogto.com/slide-shows/images/316ff2cc-8580-4da7-92a3-01c144c39b3b.jpg?quality=70&w=2184',-79.400446, 43.645394);
+
+INSERT INTO locations (user_id, Description, title, address, phone, image, long, lat)
+VALUES (1, 'Creative Latin dishes & cocktails', 'Baro', '485 King St W', '416-363-8388', 'https://media.blogto.com/slide-shows/images/c1ad4c17-2f1f-4846-ac6a-97716c6e01ca.jpg?quality=70&w=2184',-79.397043, 43.645066);
+
+INSERT INTO locations (user_id, Description, title, address, phone, image, long, lat)
+VALUES (1, 'Patio Tapas at its Best', 'Mezu', '1375 Dundas St W', '647-346-8858', 'https://media.blogto.com/slide-shows/images/61613fc9-a0b6-4463-8dd7-8cead1660e20.jpg?quality=70&w=2184',-79.427902, 43.649537);
+
+INSERT INTO locations (user_id, Description, title, address, phone, image, long, lat)
+VALUES (1, 'Irish-inspired saloon with a backyard patio & upscale pub fare', 'Allens', '143 Danforth Ave', '416-463-3086', 'https://media.blogto.com/slide-shows/images/308fd450-8995-4413-8b48-2264e96d5f93.jpg?quality=70&w=2184',-79.357332, 43.676488);
+
+INSERT INTO locations (user_id, Description, title, address, phone, image, long, lat)
+VALUES (1, 'Canadian-French Bistro with Classic Fare', 'Union', '72 Ossington Ave', '416-850-0093', 'https://media.blogto.com/slide-shows/images/896a3940-f28c-4c6d-abf2-e1a14162d6f7.jpg?quality=70&w=2184',-79.419461, 43.645841);
+
+INSERT INTO locations (user_id, Description, title, address, phone, image, long, lat)
+VALUES (1, 'Japanese and Peruvian Cuisine under the Sun', 'Chotto Matte', '161 Bay St', '416-988-8097', 'https://media.blogto.com/slide-shows/images/1abe748d-6f40-4c59-b73f-39ba09d40bd0.jpg?quality=70&w=2184',-79.379264, 43.646353);
+
+INSERT INTO locations (user_id, Description, title, address, phone, image, long, lat)
+VALUES (1, 'Eatery serving Wood-fired Pizza, Pasta & Traditional Italian mains.', 'Piano Piano Restaurant', '88 Harbord St', '416-929-7788', 'https://media.blogto.com/slide-shows/images/e7600f96-4b8e-4abc-9e4e-1e75fabe217b.jpg?quality=70&w=2184',-79.402869, 43.662913);
+
+INSERT INTO locations (user_id, Description, title, address, phone, image, long, lat)
+VALUES (1, 'Classic French plates & old world wines in an unpretentious setting.', 'Le Phenix', '1263 Queen St W', '416-342-1587', 'https://media.blogto.com/slide-shows/images/397579e6-6fc1-4f16-9104-f373ec00c300.jpg?quality=70&w=2184',-79.431138, 43.641708);
