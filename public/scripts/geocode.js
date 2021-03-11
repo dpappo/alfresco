999f112c0a3c93ed3a2674a883c2716b323fcd4b
Jesse <jessedaniel@live.ca>
2021-03-11 17:37:05 +0000

    changed to mapbox geo w/ @dpappo

--- public/scripts/geocode.js
+++ public/scripts/geocode.js
@@ -1,76 +1,59 @@
-// A $( document ).ready() block.
-
-const geocodeToList = function(geocodeResult) {
+const geocodeToList = function (geocodeResult) {
   let insertString = ""
-  //console.log("geocodeResult: ", geocodeResult)
   for (let i = 0; i < geocodeResult.length; i++) {
-    insertString += `<li data-lat="${geocodeResult[i].latitude}" 
-    data-long="${geocodeResult[i].longitude}" 
-    data-title="${geocodeResult[i].name}" 
-    data-label="${geocodeResult[i].label}" 
-    
+    insertString += `<li data-lat="${geocodeResult[i].geometry.coordinates[1]}"
+    data-long="${geocodeResult[i].geometry.coordinates[0]}"
+    data-title="${geocodeResult[i].text}"
+    data-label="${geocodeResult[i].place_name}"
+
     class="mytarget ${i}">
-    
-    ${geocodeResult[i].label} 
+
+    ${geocodeResult[i].place_name}
     <button type="button" class="btn btn-success btn-sm">Select</button>
-    
+
     </li>`
   }
 
   return insertString;
 }
 
-    //const mapboxAPI = config.MAPBOX;
-    //const positionAPI = config.POSITION;
 
 
 
 const geocodeAPI = (address, callback) => {
-  const APIKey = "acecc6c6b153f98979acfc86867bc5a2";
+  const APIKey = 'pk.eyJ1IjoiZHBhcHBvIiwiYSI6ImNrbTEwNmI4dzAxeXMyeGp2c2VyajBwa20ifQ.U6SgB3CiYWT8xwZSv0eYsQ';
   const query = address;
-  const endpoint = `http://api.positionstack.com/v1/forward?access_key=${APIKey}&callback=FUNCTION_NAME&query=${query}&limit=5&country=CA`;
-  console.log(endpoint);
+  const endpoint = `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?country=CA&access_token=${APIKey}`;
   return $.ajax({
     url: endpoint,
     dataType: 'json',
-            
-    success: function(data) {
-      callback(data.data)
-  }
-})
+
+    success: function (data) {
+      console.log('features:', data.features);
+      callback(data.features);
+    }
+  })
 }
 
-$( document ).ready(function() {
+$(document).ready(function () {
 
-$("#addressinput").on("input", $.debounce(300, function(evt) {
-  
-  
-  $(".address-insert").empty();
-  geocodeAPI($("#addressinput").val(), function(data) {
-    const allListings = geocodeToList(data);
-    $(".address-insert").append(allListings)
-  
-  $(".mytarget").click(function(){
-    $("#titleinput").val($(this).attr("data-title"))
-    $("#lat").val($(this).attr("data-lat"))
-    $("#long").val($(this).attr("data-long"))
-    $("#addressinput").val($(this).attr("data-label"))
+  $("#addressinput").on("input", $.debounce(300, function (evt) {
+
+
     $(".address-insert").empty();
+    geocodeAPI($("#addressinput").val(), function (data) {
+      const allListings = geocodeToList(data);
+      $(".address-insert").append(allListings)
 
-  })
-  })
+      $(".mytarget").click(function () {
+        $("#titleinput").val($(this).attr("data-title"))
+        $("#lat").val($(this).attr("data-lat"))
+        $("#long").val($(this).attr("data-long"))
+        $("#addressinput").val($(this).attr("data-label"))
+        $(".address-insert").empty();
 
-
-
-
-
-
-}))
-
-
-
-
-
-
+      })
+    })
+  }))
 });
 
